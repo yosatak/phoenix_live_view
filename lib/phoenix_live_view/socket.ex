087bedfcf455595c4c43f5a3139e1c0a3c8d8772
Yoshitaka Takeuchi <yosh@baud.jp>
2020-04-01 13:45:07 +0900

    Update socket.ex

--- lib/phoenix_live_view/socket.ex
+++ lib/phoenix_live_view/socket.ex
@@ -1,10 +1,8 @@
 defmodule Phoenix.LiveView.Socket do
-  @moduledoc """
-  The LiveView socket for Phoenix Endpoints.
-  """
   use Phoenix.Socket
-  require Logger
 
+  require Logger
+  
   if Version.match?(System.version(), ">= 1.8.0") do
     @derive {Inspect,
              only: [:id, :endpoint, :router, :view, :parent_pid, :root_pid, :assigns, :changed]}
@@ -26,10 +24,11 @@ defmodule Phoenix.LiveView.Socket do
 
   @type t :: %__MODULE__{}
   @type unsigned_params :: map
-  @type assigns :: map
+  @type assigns :: map | Phoenix.LiveView.Socket.AssignsNotInSocket.t()
 
   channel "lv:*", Phoenix.LiveView.Channel
 
+  
   @doc """
   Connects the Phoenix.Socket for a LiveView client.
   """
@@ -37,33 +36,30 @@ defmodule Phoenix.LiveView.Socket do
   def connect(_params, %Phoenix.Socket{} = socket, connect_info) do
     case connect_info do
       %{session: session} when is_map(session) ->
-        {:ok, put_in(socket.private[:session], session)}
+	case Guardian.Phoenix.Socket.authenticate(socket, Elenchos.Authentication.Guardian, session["guardian_default_token"]) do
+	  {:ok, authed_socket} ->
+	    IO.inspect(put_in(authed_socket.private[:session], session))
+	    {:ok, put_in(authed_socket.private[:session], session)}
 
+	  {:error, _} ->
+	    {:ok, put_in(socket.private[:session], session)}
+	end
+	  
       %{session: _} ->
         Logger.debug("""
         LiveView session was misconfigured or the user token is outdated.
-
         1) Ensure your session configuration in your endpoint is in a module attribute:
-
             @session_options [
               ...
             ]
-
         2) Change the `plug Plug.Session` to use said attribute:
-
             plug Plug.Session, @session_options
-
         3) Also pass the `@session_options` to your LiveView socket:
-
             socket "/live", Phoenix.LiveView.Socket,
               websocket: [connect_info: [session: @session_options]]
-
         4) Define the CSRF meta tag inside the `<head>` tag in your layout:
-
             <%= csrf_meta_tag() %>
-
         5) Pass it forward in your app.js:
-
             let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
             let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
         """)
@@ -79,5 +75,9 @@ defmodule Phoenix.LiveView.Socket do
   Identifies the Phoenix.Socket for a LiveView client.
   """
   @impl Phoenix.Socket
-  def id(socket), do: socket.private[:session]["live_socket_id"]
+  def id(socket) do
+    if Guardian.Phoenix.Socket.authenticated?(socket) do
+      "users_socket:"<>Guardian.Phoenix.Socket.current_resource(socket).id
+    end
+  end
 end
